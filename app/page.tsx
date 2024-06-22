3d85a5e4e7570f8185100411bd2a126187019d80
Michael Tseng <michael08120@gmail.com>
2024-06-22 11:33:46 -0700

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
+import UploadApp from "../components/ui/upload";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      
+      <UploadApp />
     </div>
   );
 }
--- components/ui/upload.tsx
+++ components/ui/upload.tsx
@@ -25,12 +25,12 @@ const props: UploadProps = {
   },
 };
 
-const App: React.FC = () => (
+const UploadApp: React.FC = () => (
   <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
+    <p className="flex justify-center">
       <Inbox />
     </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+    <p className="text-sm">Click or drag file to this area to upload</p>
     <p className="ant-upload-hint">
       Support for a single or bulk upload. Strictly prohibited from uploading company data or other
       banned files.
@@ -38,4 +38,4 @@ const App: React.FC = () => (
   </Dragger>
 );
 
-export default App;
+export default UploadApp;
