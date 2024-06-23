76d8ae49d9530f4cd78dfc88633e92bdad677813
Michael Tseng <michael08120@gmail.com>
2024-06-23 10:33:20 -0700

    style

--- app/packages/[id]/page.tsx
+++ app/packages/[id]/page.tsx
@@ -42,36 +42,39 @@ const ImageModal = ({
   onClose: () => boolean;
 }) => (
   <div
-    className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-    onClick={onClose}
-  >
-    <div className="relative max-w-4xl max-h-[90vh] w-full mx-4">
-      <img
-        src={`${image}`}
-        alt="Expanded view"
-        className="w-full h-full object-contain"
-      />
-      <button
-        onClick={onClose}
-        className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-colors"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div className="relative max-w-xl max-h-[100vh] w-full mx-4">
+        <img
+          src={image}
+          alt="Expanded view"
+          className="w-full h-full object-contain"
+        />
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onClose();
+          }}
+          className="absolute top-2 right-2 text-white bg-black bg-opacity-50 rounded-full p-1 hover:bg-opacity-75 transition-colors"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-  </div>
 );
 
 export default function PackagePage({ params }: { params: { id: string } }) {
